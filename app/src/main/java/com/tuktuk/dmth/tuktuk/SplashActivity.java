07b4b96cc7b95a8203b19601ec8e1cea55727a6d
knadikari <tcdlpds@gmail.com>
2016-09-09 14:23:18 +0530

    Add Internet and GPS checking functionality. And Proceed to Next View

--- app/src/main/java/com/tuktuk/dmth/tuktuk/SplashActivity.java
+++ app/src/main/java/com/tuktuk/dmth/tuktuk/SplashActivity.java
@@ -1,6 +1,13 @@
 package com.tuktuk.dmth.tuktuk;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +15,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.tuktuk.dmth.tuktuk.Activity.Registration;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,6 +27,15 @@ public class SplashActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        if(enableServices()){
+            proceedToNextView();
+        }
+        else{
+
+        }
+
+
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +45,7 @@ public class SplashActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +68,92 @@ public class SplashActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void proceedToNextView(){
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Registration.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+    }
+
+
+    private boolean enableServices(){
+        if(!checkforGps()){
+            buildAlertMessageNoService(0);//0 for GPS
+        }
+        if(checkforGps()){
+            if (!isNetworkConnected()) {
+                buildAlertMessageNoService(1);//1 for NET
+            }
+        }
+
+        boolean isrequirementdone=checkforGps();
+        isrequirementdone=isrequirementdone && checkforGps();
+        return isrequirementdone;
+    }
+    private boolean checkforGps() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            return false;
+        }
+        return true;
+    }
+
+        private void buildAlertMessageNoService(int num) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            String msg="";
+            final String Intentname;
+
+            if (num==0){// 0 Means GPS not avaialable
+                msg="Your GPS seems to be disabled, do you want to enable it?";
+                Intentname=android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS;
+            }
+            else{
+                msg="Your Internet link seems to be disabled, do you want to enable it?";
+                Intentname= Settings.ACTION_SETTINGS;
+            }
+
+            builder.setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            startActivity(new Intent(Intentname));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+
 }
